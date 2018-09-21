21a3d56ca5f4a21297a53b9a2d47dd23c3493797
Machoman <rbunnicelli2010@yahoo.com>
2018-09-20 22:39:45 -0400

    orm done

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
